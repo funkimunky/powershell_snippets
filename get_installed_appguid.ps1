16a2c22b1d6d7fb9b34cbcae27f2ac04282c3886
David Thomson <davidthomson123@hotmail.com>
2023-01-18 10:08:35 +0000

    Installed applications

---
+++ get_installed_appguid.ps1
@@ -0,0 +1,3 @@
+﻿Get-CimInstance -class Win32Reg_AddRemovePrograms  | Where-Object{ 
+    $_.DisplayName -ilike "Tek*"
+} | Format-Table DisplayName, ProdID, Publisher, Version -AutoSize
---
+++ swtich_ps_user.ps1
@@ -0,0 +1,18 @@
+﻿function switch-psuser {
+    
+    Param(
+        [Parameter(Position=0)]
+        [ValidateSet("adminsystem","administrator")]
+        $User = "adminsystem"
+    )
+
+    switch($User)
+    {
+        'adminsystem'   { $username = "pkc\dwthomson" ; $pw = "My new job in IT 88"}
+        'administrator' { $username = "pkc\apps2" ; $pw = "This is now IT 9" }
+    }
+
+    $password = $pw | ConvertTo-SecureString -AsPlainText -Force
+    $cred = New-Object System.Management.Automation.PSCredential -ArgumentList $username,$password
+    New-PSSession -Credential $cred | Enter-PSSession
+}
